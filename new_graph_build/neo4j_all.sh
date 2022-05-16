89950562c3aa34b9d7ada12a669d6345afe1c1e5
weiping.wu1 <weiping.wu1@astrazeneca.com>
2022-05-16 17:49:23 +0800

    change neo4j_build_v2 to neo4j_csp_build.py change neo4j_qa_build_1.py to neo4j_qa_build.py

--- new_graph_build/neo4j_all.sh
+++ new_graph_build/neo4j_all.sh
@@ -1,8 +1,8 @@
 python neo4j_delete.py 
 # python neo4j_build.bak.py --url localhost --user neo4j --password test
 # python QA_NER.py
-python neo4j_build_v2.py 
+python neo4j_csp_build.py 
 python neo4j_table8.py 
 python neo4j_table9.py
-python neo4j_qa_build_1.py
+python neo4j_qa_build.py
 # python neo4j_group.py --url localhost --user neo4j --password test
