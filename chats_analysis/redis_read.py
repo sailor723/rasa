5d99096b4f9af63352ef4f21e89e823f58adeab5
weiping.wu1 <weiping.wu1@astrazeneca.com>
2022-05-16 18:46:20 +0800

    set DCTA MYSQL, REDIS env in redis_read.py

--- chats_analysis/redis_read.py
+++ chats_analysis/redis_read.py
@@ -1,23 +1,44 @@
 # extract redis, write to mysql and flushall redis, need to make sure mysql tracker value is longtext format
 
 from matplotlib.font_manager import json_dump
-import redis, json
+import redis, json, os
 import pandas as pd 
 from sqlalchemy import create_engine
 import pymysql
 
+DCTA_MYSQL_USER = os.getenv('DCTA_MYSQL_USER')
+DCTA_MYSQL_PWD = os.getenv('DCTA_MYSQL_PWD')
+DCTA_MYSQL_HOST = os.getenv('DCTA_MYSQL_HOST')
+DCTA_MYSQL_PORT  = int(os.getenv('DCTA_MYSQL_PORT'))
+DCTA_MYSQL_DB  = os.getenv('DCTA_MYSQL_DB')
+DCTA_MYSQL_TABLE  = os.getenv('DCTA_MYSQL_TABLE')
+
+DCTA_REDIS_HOST = os.getenv('DCTA_REDIS_HOST')
+DCTA_REDIS_PORT= int(os.getenv('DCTA_REDIS_PORT'))
+DCTA_REDIS_USER = os.getenv('DCTA_REDIS_USER')
+DCTA_REDIS_PWD = os.getenv('DCTA_REDIS_PWD')
+
 #连接数据库
 conn=pymysql.connect(
-    host="127.0.0.1",
-    port=3306,
-    user="root",
-    password="Ecc!123456",
-    database="test_db"
+    host=DCTA_MYSQL_HOST,
+    port=DCTA_MYSQL_PORT,
+    user=DCTA_MYSQL_USER,
+    password=DCTA_MYSQL_PWD,
+    database=DCTA_MYSQL_DB
 )
+
+# conn=pymysql.connect(
+#     host="127.0.0.1",
+#     port=3306,
+#     user="root",
+#     password="Ecc!123456",
+#     database="test_db"
+# )
+
 
 cursor=conn.cursor()
 
-redis_client = redis.Redis(host='localhost', password='ecc#123456', port=6379, db=0)
+redis_client = redis.Redis(host=DCTA_REDIS_HOST, password=DCTA_REDIS_PWD, port=DCTA_REDIS_PORT, db=0)
 
 # 取前缀为tracker的Key
 for redis_key in redis_client.scan_iter("tracker*"):
