49a36ca7664105a3a0e8d7eea350a04362527881
sailor723 <sailor723@gmail.com>
2022-06-15 00:40:41 +0800

    update for conversion history and BI dashboard
    change refresh run

--- chats_analysis/chats_history.py
+++ chats_analysis/chats_history.py
@@ -1,12 +1,32 @@
 # Streamlit Timeline Component Example
 
-import os
+import os, sys, subprocess
 import streamlit as st
 from streamlit_timeline import timeline
+import subprocess
 
+from datetime import datetime
+
+# get current data and time
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 
 # use full page width
 st.set_page_config(page_title="Timeline Example", layout="wide")
+
+# hide manul and footnote
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+# refresh
+if st.button('refesh'):
+     st.write(current_time)
+     subprocess.run(["bash", "History_chats.sh"])
 
 # load data
 charts_result_name = os.path.abspath('chats_result.json')
