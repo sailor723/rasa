cb020f12f7dedde80a025e73bbb2261ad7269ebf
sailor723 <sailor723@gmail.com>
2022-06-22 15:58:05 +0800

    update BI_history.sh

--- chats_analysis/BI_dashboard.sh
+++ chats_analysis/BI_dashboard.sh
@@ -1,4 +1,4 @@
 python redis_read.py 
 python chats_log_generator.py
 python chats_df_generator.py
-# streamlit run BI_dashboard.py --server.port 8501 > bi.log 2>&1 &
+streamlit run BI_dashboard.py --server.port 8501 > bi.log 2>&1 &
